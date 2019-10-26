31740782171645b5d29d2eba03d3db78d0d8d09f
KYOTA KONNO <konno@kyotar.com>
2019-10-26 20:43:34 +0900

    簡易UI作成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section id="app">
+    <header>
+      <h1>signcode</h1>
+    </header>
+
+    <main>
+      <p>メッセージ {{ output }}</p>
+      <section>
+        <transition name="fade">
+          <img src="./assets/img/logo.png" class="avatar" alt="avatar">
+        </transition>
+      </section>
+
+      <section>
+        <textarea v-model="code" disabled></textarea>
+      </section>
+
+      <section>
+        <ButtonIcon icon="⬆️" @method="addIcon" />
+        <ButtonIcon icon="➡️" @method="addIcon" />
+        <ButtonIcon icon="⬇️" @method="addIcon" />
+        <ButtonIcon icon="⬅️" @method="addIcon" />
+      </section>
+
+      <section>
+        <button @click="play">再生</button>
+      </section>
+    </main>
+
+    <footer>
+      <small>©signcode</small>
+    </footer>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ButtonIcon from './components/ButtonIcon.vue'
+import split from 'graphemesplit'
 
 export default {
   name: 'app',
+
   components: {
-    HelloWorld
-  }
+    ButtonIcon,
+  },
+
+  data() {
+    return {
+      code: '',
+      output: '',
+    }
+  },
+
+  methods: {
+    addIcon(icon) {
+      this.code = this.code + icon
+    },
+
+    play() {
+      this.command(split(this.code)[0])
+    },
+
+    command(val) {
+      switch(val) {
+        case '⬇️':
+          console.log('下')
+          break
+        default:
+          this.output = '🚧エラーです'
+      }
+    },
+  },
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  .avatar {
+    width: 100px;
+    height: auto;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+  }
 }
+
+
 </style>
---
+++ src/components/ButtonIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <button type="button" @click="method">{{ icon }}</button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonIcon',
+
+  props: {
+    icon: String
+  },
+
+  methods: {
+    method() {
+      this.$emit('method', this.icon)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
