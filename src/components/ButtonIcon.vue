1b063b6c3803db5cd5546a26511d696bf967f20a
KYOTA KONNO <konno@kyotar.com>
2019-11-17 15:43:18 +0900

    タップ感追加

--- src/components/ButtonIcon.vue
+++ src/components/ButtonIcon.vue
@@ -23,6 +23,21 @@ export default {
 
 .btn {
   font-size: 32px;
-  margin: 0 16px;
+  line-height: 1;
+  width: 64px;
+  height: 64px;
+  text-align: center;
+  vertical-align: baseline;
+  margin: 8px;
+  padding: 12px;
+  border: 1px solid $color-gray;
+  border-radius: 8px;
+  transition: all .2s ease-in;
+
+  &:active {
+    // font-size: 24px;
+    transform: scale(0.9, 0.9);
+    background-color: $color-gray;
+  }
 }
 </style>
